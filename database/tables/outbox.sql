0f1c88389cfd8f42734e8ca19497f75eac5f2205
Ari Seyhun <ariseyhun@live.com.au>
2022-11-28 17:14:05 +0800

    Fix outbox sql

--- database/tables/outbox.sql
+++ database/tables/outbox.sql
@@ -1,6 +1,17 @@
-IF current_setting('message_store.outbox', true) = 'on' THEN
-  CREATE TABLE IF NOT EXISTS outbox (
-    id UUID PRIMARY KEY REFERENCES messages,
-    global_position bigint NOT NULL
-  );
-END IF;
+CREATE OR REPLACE FUNCTION create_outbox()
+  RETURNS void
+  LANGUAGE plpgsql AS
+$func$
+BEGIN
+  IF current_setting('message_store.outbox', true) = 'on' THEN
+    CREATE TABLE IF NOT EXISTS outbox (
+      id UUID PRIMARY KEY REFERENCES messages,
+      global_position bigint NOT NULL
+    );
+  END IF;
+END
+$func$;
+
+SELECT create_outbox();
+
+DROP FUNCTION IF EXISTS create_outbox();
