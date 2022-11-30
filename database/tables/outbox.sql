141a07dd274d2304abd1ef10e815245c5958a3a4
Ari Seyhun <ariseyhun@live.com.au>
2022-11-30 19:55:06 +0800

    Revert "Fix outbox aql"
    This reverts commit 037e0913903846789fac5b1e3437bc140f036f07.

--- database/tables/outbox.sql
+++ database/tables/outbox.sql
@@ -4,8 +4,8 @@ CREATE OR REPLACE FUNCTION create_outbox()
 $func$
 BEGIN
   IF current_setting('message_store.outbox', true) = 'on' THEN
-    CREATE TABLE IF NOT EXISTS message_store.outbox (
-      id UUID PRIMARY KEY REFERENCES message_store.messages,
+    CREATE TABLE IF NOT EXISTS outbox (
+      id UUID PRIMARY KEY REFERENCES messages,
       global_position bigint NOT NULL
     );
   END IF;
